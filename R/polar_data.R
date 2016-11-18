1bf09a5f11f810aa2c9e30d2fa72027257072adc
david.carslaw <david.carslaw@york.ac.uk>
2016-11-18 15:43:25 +0000

    add polar_data dataset

---
+++ R/polar_data.R
@@ -0,0 +1,24 @@
+##' Example data for polarMap function
+##'
+##' The \code{polar_data} dataset is provided as an example dataset as part of the \code{openairmaps}
+##' package. The dataset contains hourly measurements of air pollutant
+##' concentrations, location and meteorological data.
+##'
+##' \code{polar_data} is supplied with the \code{openairmaps} package as an example
+##' dataset for use with documented examples.
+##'
+##' @name polar_data
+##' @docType data
+##' @format Data frame with example data from four sites in London in 2009.
+##' @source \code{polar_data} was compiled from data using the \code{importAURN} function from the \code{openair} package with meteorological data from the \code{worldmet} package.
+##'
+##'
+##' @keywords datasets
+##' @examples
+##'
+##'
+##' #basic structure
+##' head(polar_data)
+##'
+##'
+NULL
