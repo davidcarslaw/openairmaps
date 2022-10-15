01a61ae44c3970efe508cdfaebe533225a5162a6
Jack Davison <davison.jack.jd@gmail.com>
2022-10-15 10:53:03 +0100

    docs (dir. analysis): add description & see also
    Added actual function descriptions and "see also" sections for the directional analysis functions.

--- R/polarMap.R
+++ R/polarMap.R
@@ -1,5 +1,13 @@
 #' Bivariate polar plots on interactive leaflet maps
 #'
+#' [polarMap()] creates a \code{leaflet} map using bivariate polar plots as
+#' markers. Any number of pollutants can be specified using the \code{pollutant}
+#' argument, and multiple layers of markers can be added and toggled between
+#' using \code{control}. See [openair::polarPlot()] for more information.
+#'
+#' @seealso Directional analysis maps: [annulusMap()], [freqMap()],
+#'   [percentileMap()], [polarMap()], [pollroseMap()], [windroseMap()].
+#'
 #' @param data A data frame. The data frame must contain the data to plot a
 #'   [openair::polarPlot()], which includes wind speed (\code{ws}), wind
 #'   direction (\code{wd}), and the column representing the concentration of a
