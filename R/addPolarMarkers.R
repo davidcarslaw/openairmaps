f874ff519b6a511de37709592782f986827eec87
Jack Davison <davison.jack.jd@gmail.com>
2022-10-19 11:21:56 +0100

    style (addPolarMarkers.R)

--- R/addPolarMarkers.R
+++ R/addPolarMarkers.R
@@ -81,95 +81,96 @@ addPolarMarkers <-
            fig.width = 4,
            fig.height = 4,
            ...) {
-  # guess lat/lon
-  latlon <- assume_latlon(
-    data = data,
-    latitude = lat,
-    longitude = lng
-  )
-  lat <- latlon$latitude
-  lng <- latlon$longitude
+    # guess lat/lon
+    latlon <- assume_latlon(
+      data = data,
+      latitude = lat,
+      longitude = lng
+    )
+    lat <- latlon$latitude
+    lng <- latlon$longitude
 
-  # define plotting function
-  args <- list(...)
-  thefun <- function(...) {
-    rlang::exec(fun, !!!args, ..., annotate = FALSE)
-  }
-
-  # where to write files
-  icon_dir <- tempdir()
-
-  save_icon <-
-    function(data,
-             fun,
-             dir,
-             pollutant,
-             fig.width,
-             fig.height,
-             ...) {
-      grDevices::png(
-        filename = paste0(dir, "/", data[[lat]][1], data[[lng]][1], "_", pollutant, ".png"),
-        width = fig.width * 300,
-        height = fig.height * 300,
-        res = 300,
-        bg = "transparent"
-      )
-
-      plt <- fun(
-        data,
-        key = FALSE,
-        pollutant = pollutant,
-        par.settings = list(axis.line = list(col = "transparent"))
-      )
-
-      grDevices::dev.off()
+    # define plotting function
+    args <- list(...)
+    thefun <- function(...) {
+      rlang::exec(fun, !!!args, ..., annotate = FALSE)
     }
 
-  # go through all sites and make some plot
-  data %>%
-    dplyr::group_split(.data[[lat]], .data[[lng]]) %>%
-    purrr::walk(
-      .f = ~ save_icon(
-        data = .x,
-        fun = thefun,
-        dir = icon_dir,
-        pollutant = pollutant,
-        fig.width = fig.width,
-        fig.height = fig.height
+    # where to write files
+    icon_dir <- tempdir()
+
+    save_icon <-
+      function(data,
+               fun,
+               dir,
+               pollutant,
+               fig.width,
+               fig.height,
+               ...) {
+        grDevices::png(
+          filename = paste0(dir, "/", data[[lat]][1], data[[lng]][1], "_", pollutant, ".png"),
+          width = fig.width * 300,
+          height = fig.height * 300,
+          res = 300,
+          bg = "transparent"
+        )
+
+        plt <- fun(
+          data,
+          key = FALSE,
+          pollutant = pollutant,
+          par.settings = list(axis.line = list(col = "transparent"))
+        )
+
+        grDevices::dev.off()
+      }
+
+    # go through all sites and make some plot
+    data %>%
+      dplyr::group_split(.data[[lat]], .data[[lng]]) %>%
+      purrr::walk(
+        .f = ~ save_icon(
+          data = .x,
+          fun = thefun,
+          dir = icon_dir,
+          pollutant = pollutant,
+          fig.width = fig.width,
+          fig.height = fig.height
+        )
       )
+
+    # definition of 'icons' aka the openair plots
+    leafIcons <-
+      lapply(sort(paste0(
+        icon_dir, "/", unique(data[[lat]]), unique(data[[lng]]), "_", pollutant, ".png"
+      )),
+      leaflet::makeIcon,
+      iconWidth = iconWidth,
+      iconHeight = iconHeight
+      )
+
+    names(leafIcons) <- paste0(unique(data[[lat]]), unique(data[[lng]]))
+    class(leafIcons) <- "leaflet_icon_set"
+
+    plot_data <-
+      dplyr::group_by(data, .data[[lat]], .data[[lng]]) %>%
+      dplyr::slice(n = 1) %>%
+      dplyr::arrange(.data[[lat]], .data[[lng]])
+
+    if (!is.null(label)) label <- plot_data[[label]]
+    if (!is.null(popup)) popup <- plot_data[[popup]]
+
+    map <- leaflet::addMarkers(
+      map,
+      data = plot_data,
+      lng = plot_data[[lng]],
+      lat = plot_data[[lat]],
+      icon = leafIcons,
+      popup = popup,
+      label = label,
+      group = group,
+      layerId = layerId
     )
 
-  # definition of 'icons' aka the openair plots
-  leafIcons <-
-    lapply(sort(paste0(
-      icon_dir, "/", unique(data[[lat]]), unique(data[[lng]]), "_", pollutant, ".png"
-    )),
-    leaflet::makeIcon,
-    iconWidth = iconWidth,
-    iconHeight = iconHeight)
-
-  names(leafIcons) <- paste0(unique(data[[lat]]), unique(data[[lng]]))
-  class(leafIcons) <- "leaflet_icon_set"
-
-  plot_data <-
-    dplyr::group_by(data, .data[[lat]], .data[[lng]]) %>%
-    dplyr::slice(n = 1) %>%
-    dplyr::arrange(.data[[lat]], .data[[lng]])
-
-  if (!is.null(label)) label <- plot_data[[label]]
-  if (!is.null(popup)) popup <- plot_data[[popup]]
-
-  map <- leaflet::addMarkers(
-    map,
-    data = plot_data,
-    lng = plot_data[[lng]],
-    lat = plot_data[[lat]],
-    icon = leafIcons,
-    popup = popup,
-    label = label,
-    group = group,
-    layerId = layerId
-  )
-
-  map
-}
+    map
+  }
