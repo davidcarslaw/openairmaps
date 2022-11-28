52dfd1a0096d9338a6f3dcb2cc5c9668151f7d91
Jack Davison <davison.jack.jd@gmail.com>
2022-11-28 13:26:16 +0000

    feat (buildPopup.R): add "control" argument
    Allow buildPopup() to split its output into different groups, for use in the "control" argumnet of e.g. polarMap().

--- R/buildPopup.R
+++ R/buildPopup.R
@@ -11,17 +11,20 @@
 #'   will go on to be used in a function such as [polarMap()].
 #' @param cols A character vector of column names, the data from which will
 #'   appear in the popup.
-#' @param latitude The decimal latitude. If not provided, latitude will be
-#'   automatically inferred from data by looking for a column named \dQuote{lat}
-#'   or \dQuote{latitude} (case-insensitively).
-#' @param longitude The decimal longitude. If not provided, longitude will be
-#'   automatically inferred from data by looking for a column named
-#'   \dQuote{lon}, \dQuote{lng}, \dQuote{long}, or \dQuote{longitude}
+#' @param latitude,longitude The decimal latitude/longitude. If not provided,
+#'   will be automatically inferred from data by looking for a column named
+#'   \dQuote{lat}/\dQuote{latitude} or
+#'   \dQuote{lon}/\dQuote{lng}/\dQuote{long}/\dQuote{longitude}
 #'   (case-insensitively).
 #' @param names Optional. A named vector used to rename certain columns in the
 #'   popups. See the Example for more information.
-#' @param fun.character A function to summarise character columns. Defaults to
-#'   collapsing unique values into a comma-separated list.
+#' @param control Optional. Column which will be used for the \code{control}
+#'   argument of other mapping functions. This only needs to be used if
+#'   \code{control} is going to be used in [polarMap()] or another similar
+#'   function, and you'd expect different values for the different map layers
+#'   (for example, if you are calculating a mean pollutant concentration).
+#' @param fun.character A function to summarise character and factor columns.
+#'   Defaults to collapsing unique values into a comma-separated list.
 #' @param fun.numeric A function to summarise numeric columns. Defaults to
 #'   taking the mean to three significant figures.
 #' @param fun.dttm A function to summarise date columns. Defaults to presenting
@@ -45,6 +48,7 @@ buildPopup <-
            latitude = NULL,
            longitude = NULL,
            names = NULL,
+           control = NULL,
            fun.character = function(x) paste(unique(x), collapse = ", "),
            fun.numeric = function(x) signif(mean(x, na.rm = TRUE), 3),
            fun.dttm = function(x) paste(lubridate::floor_date(range(x, na.rm = TRUE), "day"), collapse = " to ")) {
@@ -57,45 +61,56 @@ buildPopup <-
     latitude <- latlon$latitude
     longitude <- latlon$longitude
 
-    # multiple columns
-    summary <-
-      data %>%
-      dplyr::select(dplyr::all_of(c(latitude, longitude, cols))) %>%
-      dplyr::group_by(.data[[latitude]], .data[[longitude]]) %>%
-      dplyr::summarise(dplyr::across(tidyselect::where(is.character), fun.character),
-        dplyr::across(tidyselect::where(is.numeric), fun.numeric),
-        dplyr::across(tidyselect::where(lubridate::is.POSIXct), fun.dttm),
-        .groups = "drop"
-      )
+    make_popup <- function(data){
+      # multiple columns
+      summary <-
+        data %>%
+        dplyr::select(dplyr::all_of(c(latitude, longitude, cols))) %>%
+        dplyr::group_by(.data[[latitude]], .data[[longitude]]) %>%
+        dplyr::summarise(dplyr::across(tidyselect::where(is.character)|tidyselect::where(is.factor), fun.character),
+                         dplyr::across(tidyselect::where(is.numeric), fun.numeric),
+                         dplyr::across(tidyselect::where(lubridate::is.POSIXct), fun.dttm),
+                         .groups = "drop"
+        )
 
-    if (!is.null(names)) {
-      summary <- dplyr::rename(summary, dplyr::all_of(names))
+      if (!is.null(names)) {
+        summary <- dplyr::rename(summary, dplyr::all_of(names))
+      }
+
+      out <-
+        summary %>%
+        dplyr::select(-dplyr::all_of(c(latitude, longitude))) %>%
+        purrr::imodify(.f = ~ stringr::str_glue("<b>{.y}</b>: {.x}"))
+
+      if (typeof(out) == "list") {
+        out <- as.data.frame(out)
+        names(out) <- cols
+      }
+
+      out <-
+        out %>%
+        dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = quickTextHTML)) %>%
+        dplyr::rowwise() %>%
+        dplyr::mutate(
+          popup = paste(dplyr::c_across(), collapse = "<br>"),
+          .keep = "unused"
+        )
+
+      out[[latitude]] <- summary[[latitude]]
+      out[[longitude]] <- summary[[longitude]]
+
+      out <-
+        dplyr::left_join(data, out, by = c(latitude, longitude))
+
+      return(out)
     }
 
-    out <-
-      summary %>%
-      dplyr::select(-dplyr::all_of(c(latitude, longitude))) %>%
-      purrr::imodify(.f = ~ stringr::str_glue("<b>{.y}</b>: {.x}"))
-
-    if (typeof(out) == "list") {
-      out <- as.data.frame(out)
-      names(out) <- cols
+    if (!is.null(control)) {
+      out <- dplyr::group_split(data, .data[[control]]) %>%
+        purrr::map_dfr(make_popup)
+    } else {
+      out <- make_popup(data)
     }
 
-    out <-
-      out %>%
-      dplyr::mutate(dplyr::across(.cols = dplyr::everything(), .fns = quickTextHTML)) %>%
-      dplyr::rowwise() %>%
-      dplyr::mutate(
-        popup = paste(dplyr::c_across(), collapse = "<br>"),
-        .keep = "unused"
-      )
-
-    out[[latitude]] <- summary[[latitude]]
-    out[[longitude]] <- summary[[longitude]]
-
-    out <-
-      dplyr::left_join(data, out, by = c(latitude, longitude))
-
-    return(out)
+    out
   }
