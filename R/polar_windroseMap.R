55bbf912eb8e7c9b8e241253d5d9771c0af04a9e
Jack Davison <davison.jack.jd@gmail.com>
2023-02-01 15:10:47 +0000

    refactor: combine map scripts
    Combine static/leaflet together now that they are so similar. May be able to reduce repetition further.

--- R/polar_windroseMap.R
+++ R/polar_windroseMap.R
@@ -1,3 +1,155 @@
+#' Wind rose plots on interactive leaflet maps
+#'
+#' [windroseMap()] creates a `leaflet` map using wind roses as markers. Multiple
+#' layers of markers can be added and toggled between using `control`.
+#'
+#' @family interactive directional analysis maps
+#'
+#' @inheritParams polarMap
+#' @param data A data frame. The data frame must contain the data to plot a
+#'   [openair::windRose()], which includes wind speed (`ws`), and wind direction
+#'   (`wd`). In addition, `data` must include a decimal latitude and longitude.
+#' @param ws.int The wind speed interval. Default is 2 m/s but for low met masts
+#'   with low mean wind speeds a value of 1 or 0.5 m/s may be better.
+#' @param breaks Most commonly, the number of break points for wind speed in
+#'   windRose. For windRose and the ws.int default of 2 m/s, the default, 4,
+#'   generates the break points 2, 4, 6, 8 m/s. Breaks can also be used to set
+#'   specific break points. For example, the argument breaks = c(0, 1, 10, 100)
+#'   breaks the data into segments <1, 1-10, 10-100, >100.
+#' @param draw.legend Should a shared legend be created at the side of the map?
+#'   Default is `TRUE`.
+#' @inheritDotParams openair::windRose -ws.int -breaks -mydata -plot -annotate
+#'   -pollutant -type -cols -key
+#' @return A leaflet object.
+#' @export
+#'
+#' @seealso the original [openair::windRose()]
+#' @seealso [windroseMapStatic()] for the static `ggmap` equivalent of
+#'   [windroseMap()]
+#'
+#' @examples
+#' \dontrun{
+#' windroseMap(polar_data,
+#'   provider = "Stamen.Toner"
+#' )
+#' }
+windroseMap <- function(data,
+                        ws.int = 2,
+                        breaks = 4,
+                        latitude = NULL,
+                        longitude = NULL,
+                        control = NULL,
+                        popup = NULL,
+                        label = NULL,
+                        provider = "OpenStreetMap",
+                        cols = "turbo",
+                        alpha = 1,
+                        key = FALSE,
+                        draw.legend = TRUE,
+                        collapse.control = FALSE,
+                        d.icon = 200,
+                        d.fig = 3.5,
+                        type = NULL,
+                        ...) {
+  if (!is.null(type)) {
+    cli::cli_warn(c(
+      "!" = "{.code type} is deprecated. Different sites are now automatically identified.",
+      "i" = "Please use {.code label} and/or {.code popup} to label sites."
+    ))
+  }
+
+  # assume lat/lon
+  latlon <- assume_latlon(data = data,
+                          latitude = latitude,
+                          longitude = longitude)
+  latitude <- latlon$latitude
+  longitude <- latlon$longitude
+
+  # need to put ws in a separate column to work with the rest of openairmaps
+  # utilities...
+  data$ws_dup <- data$ws
+
+  # prep data
+  data <-
+    prepMapData(
+      data = data,
+      type = type,
+      pollutant = "ws_dup",
+      control = control,
+      "ws",
+      "wd",
+      latitude,
+      longitude,
+      popup,
+      label
+    )
+
+  # work out breaks
+  # needs to happen before plotting to ensure same scales
+  breaks <-
+    getBreaks(breaks = breaks, ws.int = ws.int, vec = data$conc, polrose = FALSE)
+
+  # identify splitting column (defaulting to pollutant)
+  if (!is.null(control)) {
+    data[control] <- as.factor(data[[control]])
+    split_col <- control
+  } else {
+    split_col <- "pollutant_name"
+  }
+
+  # define function
+  fun <- function(data) {
+    openair::windRose(
+      data,
+      plot = FALSE,
+      ws.int = ws.int,
+      breaks = breaks,
+      cols = cols,
+      alpha = alpha,
+      key = key,
+      annotate = FALSE,
+      ...,
+      par.settings = list(axis.line = list(col = "transparent"))
+    )
+  }
+
+  # create temp directory
+  tempdir <- tempdir()
+
+  # plot and save static markers
+  plots_df <-
+    create_static_markers(
+      fun = fun,
+      data = data,
+      dir = tempdir,
+      latitude = latitude,
+      longitude = longitude,
+      split_col = split_col,
+      d.fig = d.fig
+    )
+
+  # create leaflet map
+  map <-
+    make_leaflet_map(plots_df, latitude, longitude, provider, d.icon, popup, label, split_col, collapse.control)
+
+  # add legend
+  if (draw.legend) {
+    map <-
+      leaflet::addLegend(
+        map,
+        pal = leaflet::colorBin(
+          palette = openair::openColours(cols),
+          domain = breaks,
+          bins = breaks
+        ),
+        values = breaks,
+        title = "Wind Speed"
+      )
+  }
+
+  return(map)
+}
+
 #' Wind rose plots on a static ggmap
 #'
 #' [windroseMapStatic()] creates a `ggplot2` map using wind roses as markers. As
