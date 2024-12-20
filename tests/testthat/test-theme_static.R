a60ac5c7faa44b6b740a2f5e6eacae0472d326a2
jack-davison <davison.jack.jd@gmail.com>
2024-12-20 16:18:38 +0000

    test: don't print plots

--- tests/testthat/test-theme_static.R
+++ tests/testthat/test-theme_static.R
@@ -2,6 +2,6 @@ test_that("theme can be added to a ggplot2 object", {
   options(device = "cairo")
 
   testthat::expect_no_error(
-    ggplot2::ggplot() + theme_static()
+    plt <- ggplot2::ggplot() + theme_static()
   )
 })
